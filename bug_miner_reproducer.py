1bac4516332bd44cc7b1b4c7ddef0a80f4c579ed
amir9979 <amir9979@gmail.com>
2021-02-08 01:46:21 +0200

    add

--- bug_miner_reproducer.py
+++ bug_miner_reproducer.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import git
+import time
 import os
 from subprocess import Popen
 from dir_structure import DirStructure, DirId
@@ -66,12 +67,59 @@ class BugMinerReproducer(Reproducer):
         map(lambda x: commits.setdefault(x['parent'], []).append(x), map(lambda y: y[1].to_dict(), df.iterrows()))
         for bug_data in commits:
             ans[bug_data] = BugMinerReproducer(bug_data, list(set(map(lambda x: x['testcase'], commits[bug_data]))),
-                               DirId(DirStructure(BugMinerReproducer.RESULTS_DIR), bug_data),
-                               os.path.join(BugMinerReproducer.BUG_MINER_REPOS_DIR, project_name), list(set(map(lambda x: x['diff'], commits[bug_data]))), list(set(map(lambda x: x['blamed_components'], commits[bug_data]))), list(set(map(lambda x: x['commit'], commits[bug_data]))))
+                                               DirId(DirStructure(BugMinerReproducer.RESULTS_DIR), bug_data),
+                                               os.path.join(BugMinerReproducer.BUG_MINER_REPOS_DIR, project_name),
+                                               list(set(map(lambda x: x['diff'], commits[bug_data]))),
+                                               list(set(map(lambda x: x['blamed_components'], commits[bug_data]))),
+                                               list(set(map(lambda x: x['commit'], commits[bug_data]))))
         return ans
 
 
+def git_cmds_wrapper(git_cmd, spec_repo=repo, spec_mvn_repo=None, counter=0):
+    spec_mvn_repo = mvn_repo if spec_mvn_repo == None else spec_mvn_repo
+    try:
+        git_cmd()
+    except git.exc.GitCommandError as e:
+        if 'Another git process seems to be running in this repository, e.g.' in str(e):
+            if counter > 10:
+                raise e
+            counter += 1
+            time.sleep(2)
+            git_cmds_wrapper(lambda: git_cmd(), spec_repo=spec_repo, spec_mvn_repo=spec_mvn_repo, counter=counter)
+        elif 'nothing to commit, working tree clean' in str(e):
+            pass
+        elif 'Please move or remove them before you switch branches.' in str(e):
+            git_cmds_wrapper(lambda: spec_repo.index.add('.'), spec_repo=spec_repo, spec_mvn_repo=spec_mvn_repo)
+            git_cmds_wrapper(lambda: spec_repo.git.clean('-xdf'), spec_repo=spec_repo, spec_mvn_repo=spec_mvn_repo)
+            git_cmds_wrapper(lambda: spec_repo.git.reset('--hard'), spec_repo=spec_repo, spec_mvn_repo=spec_mvn_repo)
+            time.sleep(2)
+            git_cmds_wrapper(lambda: git_cmd(), spec_repo=spec_repo, spec_mvn_repo=spec_mvn_repo)
+        elif 'already exists and is not an empty directory.' in str(e):
+            pass
+        elif 'warning: squelched' in str(e) and 'trailing whitespace.' in str(e):
+            pass
+        elif 'Filename too long' in str(e):
+            if counter > 10:
+                raise e
+            counter += 1
+            if spec_mvn_repo:
+                spec_mvn_repo.hard_clean()
+            git_cmds_wrapper(lambda: git_cmd(), spec_repo=spec_repo, spec_mvn_repo=spec_mvn_repo, counter=counter)
+        else:
+            raise e
+
+
+def clone_repo(base, url):
+    repo_url = url.geturl().replace('\\', '/').replace('////', '//')
+    git_cmds_wrapper(
+        lambda: git.Git(base).clone(repo_url)
+    )
+
+
 if __name__ == "__main__":
+    import settings
+    git_url, jira_url = settings.projects.get(sys.argv[1])
+    clone_repo(BugMinerReproducer.BUG_MINER_REPOS_DIR, git_url)
     projects = BugMinerReproducer.read_bug_miner_csv(sys.argv[1])
     for p in projects:
         projects[p].dump()
